7ed11cf7cf21c60c829eebb2c1e104c9807eddec
irisnoh <53018217+irisnoh@users.noreply.github.com>
2020-10-27 16:05:14 -0500

    delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,14 @@ class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
         # finding post and getting comments on it, getting comments model and create a new comment on that post
-        @comment = @post.comments.create(params[:comment]).permit(:name, :comment))
+        @comment = @post.comments.create(params[:comment].permit(:name, :comment))
         redirect_to post_path(@post)
     end
 
     def destroy 
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   root "posts#index"
+
+  
 end
 
