85cac73c0173ab4a46a5138b6113313af71677cc
irisnoh <53018217+irisnoh@users.noreply.github.com>
2020-10-27 14:47:03 -0500

    edit functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,22 @@ def show
     @post = Post.find(params[:id])
 end
 
+def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+        redirect_to @post
+    else 
+        render 'edit'
+    end
+
+end
+
+def edit
+    @post = Post.find(params[:id])
+end
+
+
 
     private
 
